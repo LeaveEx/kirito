194bd636cbee3646015d600e1917fb4fc51fe9fc
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2024-06-27 16:51:16 +0700

    Update mongo.py

--- YukkiMusic/core/mongo.py
+++ YukkiMusic/core/mongo.py
@@ -41,4 +41,4 @@ else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
     mongodb = _mongo_async_.mongo.py
-    pymongodb = _mongo_sync_.Kaal
+    pymongodb = _mongo_sync_.mongo.py
